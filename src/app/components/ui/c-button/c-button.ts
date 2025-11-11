a129c8f9e99c4b7bd86b1d3e5aacc634f4a8914d
HugoMengualAlcGVA <hugmenalc@alu.edu.gva>
2025-11-11 13:48:09 +0100

    añadido lo del tema arquitectura, mah o menoh

---
+++ src/app/components/ui/c-button/c-button.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-c-button',
+  imports: [MatButtonModule],
+  templateUrl: './c-button.html',
+  styleUrl: './c-button.scss',
+})
+export class CButton {
+
+}
